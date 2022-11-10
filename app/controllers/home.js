2c16e66bd2f18c77b578126385f146ef3433481e
eD3ath <edeath1208@gmail.com>
2022-11-11 00:02:14 +0800

    fix ads

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const device = require('device');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => res.render('home', { title: 'CryptoBlades Tracker', page: 'home' }));
-router.get('/calculator', (req, res, next) => res.render('calculator', { title: 'CryptoBlades Tracker - Rewards Calculator', page: 'calculator' }));
+router.get('/', (req, res, next) => {
+  const mydevice = device(req.get('User-Agent'));
+  res.render('home', { title: 'CryptoBlades Tracker', page: 'home', isPhone: mydevice.is('phone') });
+});
+router.get('/calculator', (req, res, next) => {
+  const mydevice = device(req.get('User-Agent'));
+  res.render('calculator', { title: 'CryptoBlades Tracker - Rewards Calculator', page: 'calculator', isPhone: mydevice.is('phone') });
+});
 // router.get('/stats', (req, res, next) => res.render('stats', { title: 'CryptoBlades Tracker - Statistics', page: 'stats' }));
-router.get('/diagnostic', (req, res, next) => res.render('diagnostic', { title: 'Tools - Diagnostic', page: 'diagnostic' }));
-router.get('/market', (req, res, next) => res.render('market', { title: 'Tools - Market Sales Tracker', page: 'market' }));
+router.get('/diagnostic', (req, res, next) => {
+  const mydevice = device(req.get('User-Agent'));
+  res.render('diagnostic', { title: 'Tools - Diagnostic', page: 'diagnostic', isPhone: mydevice.is('phone') });
+});
+router.get('/market', (req, res, next) => {
+  const mydevice = device(req.get('User-Agent'));
+  res.render('market', { title: 'Tools - Market Sales Tracker', page: 'market', isPhone: mydevice.is('phone') });
+});
 
 module.exports = router;
